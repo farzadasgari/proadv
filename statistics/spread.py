9b765973bda736691865ab98498ac7cc947b859d
Narges Yaghoubi <146190598+nargesyaghoubi@users.noreply.github.com>
2024-04-27 23:25:20 +0430

    Compute the Standard deviation

--- statistics/spread.py
+++ statistics/spread.py
@@ -9,7 +9,7 @@ def variance(data):
     The variance is computed for the flattened array by default, otherwise over the specified axis.
 
     Parameters
-    ----------
+    -------
     data (np.ndarray): The 1D array of data for which to calculate the variance. 
        
 
@@ -18,12 +18,12 @@ def variance(data):
     _var_
     
     Raises
-    ------
+    -------
     TypeError
         "String cannot be placed as an element of an array"
 
     Examples
-    ------
+    -------
     >>> data =  np.array([14, 8, 11, 10])
     >>> np.var(data)
     out : 4.6875
@@ -47,5 +47,43 @@ def variance(data):
 
 
 def std(data):
-    # Implement the logic to calculate standard deviation of the data array
-    pass
+    '''
+    Compute the standard deviation along the specified axis.
+
+    Returns the standard deviation, a measure of the spread of a distribution, 
+    of the array elements. The standard deviation is computed for the flattened array 
+    by default, otherwise over the specified axis.
+
+    Parameters
+    -------
+    data (np.ndarray): The 1D array of data for which to calculate the Standard deviation. 
+
+    Returns
+    -------
+    _std_ : ndarray
+        If out is None, return a new array containing the standard deviation, 
+        otherwise return a reference to the output array. 
+
+    Raises
+    -------
+    TypeError
+        "String cannot be placed as an element of an array"
+
+    Examples
+    -------
+    >>> data =  np.array([14, 8, 11, 10, 5, 7])
+    out : 2.9107081994288304
+    '''
+    try :
+        if data.ndim != 1:  # Optional check for 1D array
+            raise ValueError("Data array must be a 1D array.")
+        _std_ = np.std(data)
+        for element in data:
+            if isinstance(element , str):
+            # isinstance returns True if the specified object is of the specified type, otherwise False. 
+                raise TypeError ("String cannot be placed as an element of an array")
+        return _std_
+    except TypeError as TE:
+        print(f"Type Error: {TE}")
+    except ValueError as VE:
+        print(f"Value Error:{VE}")
