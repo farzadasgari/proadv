e37df31e329147bc79ef5663538e4c11770356ef
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-04-27 15:19:22 +0330

    Modify moving_average docstrings

--- statistics/series.py
+++ statistics/series.py
@@ -3,42 +3,42 @@ import numpy as np
 
 def moving_average(data, window_size=20):
     """
-        Calculates the moving average of a 1D array.
+    Calculates the moving average of a 1D array.
 
-        Parameters
-        ------
-        data (np.ndarray): The 1D array of data for which to calculate the moving average.
-        window_size (int, optional): The size of the window for the moving average.
-            Defaults to 20. Must be less than or equal to the size of the data array.
+    Parameters
+    ------
+    data (np.ndarray): The 1D array of data for which to calculate the moving average.
+    window_size (int, optional): The size of the window for the moving average.
+        Defaults to 20. Must be less than or equal to the size of the data array.
 
-        Returns
-        ------
-        np.ndarray: The moving average of the data, with the same shape as the input array.
+    Returns
+    ------
+    np.ndarray: The moving average of the data, with the same shape as the input array.
 
-        Raises
-        ------
-        ValueError: If the window_size is larger than the size of the data array.
+    Raises
+    ------
+    ValueError: If the window_size is larger than the size of the data array.
 
-        Notes
-        ------
-        This function implements a cumulative moving average calculation. It's more
-        efficient than calculating a simple moving average for each element.
+    Notes
+    ------
+    This function implements a cumulative moving average calculation. It's more
+    efficient than calculating a simple moving average for each element.
 
-        Examples
-        ------
-        >>> import proadv as adv  # Option 1: Full import path
-        >>> import numpy as np
-        >>> data = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        >>> ma = adv.statistics.series.moving_average(data, window_size=3)
-        >>> print(ma)
-        [1.0 1.5 2.0 2.7 3.3 4.0 4.7 5.3 6.0 6.7]
+    Examples
+    ------
+    >>> import proadv as adv  # Option 1: Full import path
+    >>> import numpy as np
+    >>> data = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    >>> ma = adv.statistics.series.moving_average(data, window_size=3)
+    >>> print(ma)
+    [1.0 1.5 2.0 2.7 3.3 4.0 4.7 5.3 6.0 6.7]
 
-        ------
+    ------
 
-        >>> from proadv.statistics.series import moving_average  # Option 2: Direct import
-        >>> import numpy as np
-        >>> data = np.random.rand(300)
-        >>> ma = moving_average(data)
+    >>> from proadv.statistics.series import moving_average  # Option 2: Direct import
+    >>> import numpy as np
+    >>> data = np.random.rand(300)
+    >>> ma = moving_average(data)
     """
 
     if window_size <= 0:
@@ -63,13 +63,13 @@ def moving_average(data, window_size=20):
     # Efficiently calculate the moving average for remaining elements using cumulative sum
     for i in range(window_size, data.size):
         """
-            This loop calculates the moving average for elements from index 'window_size' onwards.
-            It leverages the previously calculated moving average (ma[i-1]) and the new data point
-            (data[i]) to efficiently update the moving average using the formula:
-            ma[i] = ma[i-1] + (data[i] - data[i-window_size]) / window_size
-            
-            Update the moving average based on the previous average, new data point,
-            and the difference between the current and window_sizeth previous data point
+        This loop calculates the moving average for elements from index 'window_size' onwards.
+        It leverages the previously calculated moving average (ma[i-1]) and the new data point
+        (data[i]) to efficiently update the moving average using the formula:
+        ma[i] = ma[i-1] + (data[i] - data[i-window_size]) / window_size
+        
+        Update the moving average based on the previous average, new data point,
+        and the difference between the current and window_sizeth previous data point
        """
         ma[i] = ma[i - 1] + (data[i] - data[i - window_size]) / window_size
 
