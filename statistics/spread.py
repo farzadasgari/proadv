47fd4c8eb2ecba44c11583345b8374a2e1756232
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-04-27 15:18:32 +0330

    Initializing spread statistical functions

---
+++ statistics/spread.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def variance(data):
+    # Implement the logic to calculate variance of the data array
+    pass
+
+
+def std(data):
+    # Implement the logic to calculate standard deviation of the data array
+    pass
