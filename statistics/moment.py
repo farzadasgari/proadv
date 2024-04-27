bf1265fad04f6cdb0c1918728f0e8dfba594695b
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-04-27 15:15:14 +0330

    Initializing momentums statistical functions

---
+++ statistics/moment.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def skewness(data):
+    # Implement the logic to calculate skewness of the data array
+    pass
+
+
+def kurtosis(data):
+    # Implement the logic to calculate kurtosis of the data array
+    pass
