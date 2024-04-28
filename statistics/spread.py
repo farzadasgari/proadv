2535ca802c3f095b8ad955624adfd4c53f6f1222
Narges Yaghoubi <146190598+nargesyaghoubi@users.noreply.github.com>
2024-04-29 01:44:06 +0430

    Compute the Variance

--- statistics/spread.py
+++ statistics/spread.py
@@ -2,8 +2,52 @@ import numpy as np
 
 
 def variance(data):
-    # Implement the logic to calculate variance of the data array
-    pass
+    '''
+    Compute the variance along the specified axis.
+
+    Returns the variance of the array elements, a measure of the spread of a distribution. 
+    The variance is computed for the flattened array by default, otherwise over the specified axis.
+
+    Parameters
+    -------
+    data (np.ndarray): The 1D array of data for which to calculate the variance. 
+       
+
+    Returns
+    -------
+    _var_
+    
+    Raises
+    -------
+    TypeError:
+        "String cannot be placed as an element of an array."
+
+    ValueError:
+        "Data array must be a 1D array." 
+
+
+    Examples
+    -------
+    >>> data = [14, 8, 11, 10]
+    >>> variance(data)
+    >>> out : 4.6875
+
+    '''
+    try :
+        if data.ndim != 1:  # Optional check for 1D array
+            raise ValueError("Data array must be a 1D array.")
+        _var_ = np.var(data)
+        
+        for element in data:
+            if isinstance(element , str):
+            # isinstance returns True if the specified object is of the specified type, otherwise False. 
+                raise TypeError ("String cannot be placed as an element of an array")
+        return _var_
+            
+    except TypeError as TE:
+        print(f"Type Error: {TE}")
+    except ValueError as VE:
+        print(f"Value Error:{VE}")
 
 
 def std(data):
@@ -35,7 +79,7 @@ def std(data):
 
     Examples
     -------
-    >>> data = ([14, 8, 11, 10, 5, 7])
+    >>> data = [14, 8, 11, 10, 5, 7]
     >>> std(data)
     >>> out : 2.9107081994288304
     '''
