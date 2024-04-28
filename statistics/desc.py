1848127bf3193f9a7ab4084527f8c036927e30fc
Narges Yaghoubi <146190598+nargesyaghoubi@users.noreply.github.com>
2024-04-28 21:19:57 +0430

    Compute the Median

--- statistics/desc.py
+++ statistics/desc.py
@@ -16,6 +16,48 @@ def mean():
     pass
 
 
-def median():
-    # Implement the logic to calculate the median of the data array
-    pass
+def median(data):
+    '''
+    Compute the median along the specified axis.
+
+    Returns the median of the array elements.
+
+    Parameters
+    -------
+    data (np.ndarray): The 1D array of data for which to calculate the Median. 
+
+    Returns
+    -------
+    Calculate the median of the data
+
+    Raises
+    -------
+    TypeError
+        "String cannot be placed as an element of an array"
+
+    ValueError
+        "cannot calculate median with empty array"
+
+    Examples
+    -------
+    >>> data =  np.array([14, 8, 11, 10, 5, 7])
+    >>> np.median(data)
+    Out : 9.0
+
+    '''
+    try:
+        if data.ndim != 1:  # Optional check for 1D array
+            raise ValueError("Data array must be a 1D array.")
+
+        if isinstance(data, str) :
+            # isinstance returns True if the specified object is of the specified type, otherwise False.
+            raise TypeError ("String cannot be placed as an element of an array")
+        if np.size(data) == 0:
+            # The array cannot be empty
+            raise ValueError ("cannot calculate median with empty array")
+        _median_ = np.median(data) # Calculate the median
+        return _median_
+    except TypeError as TE :
+        print(f"Type Error: {TE}")
+    except ValueError as VE:
+        print(f"Value Error:{VE}")
