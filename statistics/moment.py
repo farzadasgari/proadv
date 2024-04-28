d9534982e563c867748095f12ffa320bde5ad6cc
Hossein <hosseinabedi134@gmail.com>
2024-04-28 22:16:52 +0330

    Added the functions Kurtosis

--- statistics/moment.py
+++ statistics/moment.py
@@ -1,4 +1,5 @@
 import numpy as np
+import desc
 
 
 def skewness(data):
@@ -6,6 +7,48 @@ def skewness(data):
     pass
 
 
-def kurtosis(data):
-    # Implement the logic to calculate kurtosis of the data array
-    pass
+def kurtosis(x):
+    """
+    Compute the kurtosis of a dataset.
+
+ Kurtosis is a measure of the "tailedness" of the probability distribution of a real-valued random variable.
+    This function calculates kurtosis using the Pearson method, which is the standardized fourth central moment.
+
+    Parameters
+    ----------
+    x : array_like
+        An array containing the data points. The array will be flattened if it is not already 1-D.
+
+    Returns
+    -------
+    float
+        The kurtosis of the dataset. If the input contains NaNs, the function will return NaN.
+
+    Notes
+    -----
+    This function converts the input array to a NumPy array, calculates the mean and standard deviation of the data,
+    computes the fourth central moment, and then standardizes it to find the kurtosis.
+
+    Examples
+    --------
+    >>> data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+    >>> kurtosis(data)
+    out: 2.2
+    """
+
+    # Convert data to a numpy array
+    x = np.array(x)
+
+    # Calculate the mean
+    average = desc.mean(x)
+
+    # Calculate the standard deviation
+    standard_dev = desc.standard_deviation(x)
+
+    # Calculate the fourth central moment
+    fourth_moment = desc.mean((x - average) ** 4)
+
+    # Standardize the fourth moment
+    standardized_moment = fourth_moment / standard_dev ** 4
+
+    return standardized_moment  # Returns the standardized moment which basically is kurtosis
