d7f7cf93df51f69f423275494eb02f4b21b4dc38
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-05-02 21:08:51 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,23 @@ setup(
     author="Farzad Asgari",
     author_email="std_farzad.asgari@alumni.khu.ac.ir",
     packages=find_packages(),
-    description="post process the data of acoustic Doppler velocimeter (ADV)",
-    long_description='''A package that focuses on developing and validating the 3D-KDE filtration algorithms using 
-    Python and open-source languages. Users can upload velocity time-series data and apply the 3D-KDE algorithms''',
+    short_description="ProAdv: Process Acoustic Doppler Velocimeter data with advanced despiking and analysis tools",
+    long_description="""
+    ProAdv is a Python package designed for post-processing acoustic Doppler velocimeter (ADV) datasets. It offers a comprehensive suite of tools for denoising and despiking ADV data, ensuring reliable and accurate measurements.
+
+    **Key Features:**
+
+    * Advanced Despiking Algorithms: Employ robust methods like ACC, PST, mPST, VC, KDE, 3d-KDE, and m3d-KDE to effectively remove noise and outliers.
+    * Comprehensive Statistical Functions: Calculate essential statistical measures like minimum, maximum, mean, median, skewness, and kurtosis to gain deeper insights into your data.
+    * Additional Analysis Tools: Analyze your data further with moving average calculations, singular spectrum analysis (SSA), and pollution rate (PR) calculation.
+
+    **Benefits:**
+
+    * Improved Data Quality: ProADV's advanced despiking algorithms ensure clean and reliable ADV data, crucial for accurate scientific analysis.
+    * Enhanced Statistical Insights: Gain a deeper understanding of your data through comprehensive statistical analysis tools.
+    * Streamlined Workflow: ProAdv provides a convenient platform for post-processing and analyzing ADV datasets.
+    """,
+    long_description_content_type="text/markdown",
     install_requires=[
         "numpy", 
         "pytest",
