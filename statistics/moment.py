2ff647698c03877c0ca0fbdd21804b498b3d3148
Narges Yaghoubi <146190598+nargesyaghoubi@users.noreply.github.com>
2024-05-02 22:26:04 +0330

    Update the skewness docString

--- statistics/moment.py
+++ statistics/moment.py
@@ -8,26 +8,28 @@ def skewness(data):
     """
     Compute the sample skewness of a data set. 
 
-    For normally distributed data, the skewness should be about zero. For
-    unimodal continuous distributions, a skewness value greater than zero means
-    that there is more weight in the right tail of the distribution. The
-    function `skewtest` can be used to determine if the skewness value
-    is close enough to zero, statistically speaking.
-
     Parameters
     ------
     data (np.ndarray): The 1D array of data for which to calculate the Skewness. 
 
     Returns
     ------
-    skewness : The skewness of the data
+    skew: The skewness of the data
 
     References
     ------
-    .. [1] Zwillinger, D. and Kokoska, S. (2000). CRC Standard
+    [1] Zwillinger, D. and Kokoska, S. (2000). CRC Standard
        Probability and Statistics Tables and Formulae. Chapman & Hall: New
        York. 2000.
        Section 2.2.24.1
+
+    Notes
+    ------
+    For normally distributed data, the skewness should be about zero. For
+        unimodal continuous distributions, a skewness value greater than zero means
+        that there is more weight in the right tail of the distribution. The
+        function `skewtest` can be used to determine if the skewness value
+        is close enough to zero, statistically speaking.
 
     Examples
     ------
