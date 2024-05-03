37b146800540bed2e7f136e2f4ba645f623d70ef
Farzad Asgari <farzad.developer13@gmail.com>
2024-05-04 00:18:02 +0330

    Added linear interpolation replacement method

--- filtration/replacement/rep.py
+++ filtration/replacement/rep.py
@@ -23,3 +23,17 @@ def last_valid_data(velocities, spike_indices):
         modified_data[idx] = modified_data[idx - 1]
 
     return modified_data
+
+
+def linear_interpolation(velocities, spike_indices):
+    from proadv.statistics.desc import mean
+    modified_data = np.copy(velocities)
+    modified_data[spike_indices] = np.nan
+    for idx in spike_indices:
+        try:
+            modified_data[idx] = np.around(
+                mean(modified_data[idx - 1] + modified_data[idx:][~np.isnan(modified_data[idx:])][0]),
+                decimals=4)
+        except IndexError:
+            modified_data[idx] = mean(velocities)
+    return modified_data
