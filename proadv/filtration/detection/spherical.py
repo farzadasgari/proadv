4a4d6fcbe7c3897c5014e477a6e79e1c50d4f56a
Farzad Asgari <farzad.developer13@gmail.com>
2024-05-05 16:07:12 +0330

    Added axis rotation calculator in spherical

--- proadv/filtration/detection/spherical.py
+++ proadv/filtration/detection/spherical.py
@@ -30,13 +30,30 @@ def _gradients(c):
 
     Parameters
     ------
-    c (numpy.ndarray): Input data
+    c (numpy.ndarray): Input data.
 
     Returns
     ------
     dc (numpy.ndarray): First order derivative of the input data.
-    dc2 (numpy.ndarray): Second order derivative of the input data
+    dc2 (numpy.ndarray): Second order derivative of the input data.
     """
     dc = np.gradient(c)
     dc2 = np.gradient(dc)
     return dc, dc2
+
+
+def _rotation(c, dc, dc2, theta):
+    if theta == 0:
+        x = c.copy()
+        y = dc.copy()
+        z = dc2.copy()
+    else:
+        rotation_matrix = np.around(np.array([
+            [np.cos(theta), 0, np.sin(theta)],
+            [0, 1, 0],
+            [-np.sin(theta), 0, np.cos(theta)]
+        ]), 4)
+        x = np.around(c * rotation_matrix[0, 0] + dc * rotation_matrix[0, 1] + dc2 * rotation_matrix[0, 2], 4)
+        y = np.around(c * rotation_matrix[1, 0] + dc * rotation_matrix[1, 1] + dc2 * rotation_matrix[1, 2], 4)
+        z = np.around(c * rotation_matrix[2, 0] + dc * rotation_matrix[2, 1] + dc2 * rotation_matrix[2, 2], 4)
+    return x, y, z
