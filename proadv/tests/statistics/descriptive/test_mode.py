057033fad3c9eb93083f5c173c9cc22edba2f9ce
Hivayrn <hivahooshyrn792@gmail.com>
2024-05-06 22:42:18 +0330

    Test mode was added with numpy array

--- proadv/tests/statistics/descriptive/test_mode.py
+++ proadv/tests/statistics/descriptive/test_mode.py
@@ -16,3 +16,10 @@ def test_mode_with_tuple():
     assert mode((1, 3, 5, 7, 9, 7, 5, 9, 5, 7, 9, 7)) == (7, 4)
     assert mode((-1, -2, -4, -3, -4, -5, -6)) == (-4, 2)
     assert mode((5)) == (5, 1)
+
+def test_mode_with_numpy_array():
+    assert mode(np.array([1.5, 5.5, 5.5, 1.5, 7.5, 1.5, 8.5, 5.5, 7.5, 8.5, 5.5])) == (5.5, 4)
+    assert mode(np.array([-2.5, -4.5, -6.5, -6.5, -6.5, -8.5, -8.5])) == (-6.5, 3)
+    assert mode(np.array([1, 3, 5, 7, 9, 7, 5, 9, 5, 7, 9, 7])) == (7, 4)
+    assert mode(np.array([-1, -2, -4, -3, -4, -5, -6])) == (-4, 2)
+    assert mode(np.array([5])) == (5, 1)
