b896253d2527a3743e02d91cd9dc28c61503f7a9
Hivayrn <hivahooshyrn792@gmail.com>
2024-05-06 22:38:03 +0330

    Test mode was added with list

---
+++ proadv/tests/statistics/descriptive/test_mode.py
@@ -0,0 +1,11 @@
+# test_mode.py
+import pytest
+import numpy as np
+from proadv.statistics.descriptive import mode
+
+def test_mode_with_list():
+    assert mode([1.5, 5.5, 5.5, 1.5, 7.5, 1.5, 8.5, 5.5, 7.5, 8.5, 5.5]) == (5.5, 4)
+    assert mode([-2.5, -4.5, -6.5, -6.5, -6.5, -8.5, -8.5]) == (-6.5, 3)
+    assert mode([1, 3, 5, 7, 9, 7, 5, 9, 5, 7, 9, 7]) == (7,4)
+    assert mode([-1, -2, -4, -3, -4, -5, -6]) == (-4, 2)
+    assert mode([5]) == (5, 1)
