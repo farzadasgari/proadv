f1be544417fff0e7432fd9fc76c386e063d3e942
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-05-07 22:56:10 +0330

    Improved linear_interpolation function variables

--- proadv/filtration/replacement/replacements.py
+++ proadv/filtration/replacement/replacements.py
@@ -136,5 +136,4 @@ def linear_interpolation(velocities, spike_indices, decimals=4):
             modified_data[start:end + 1] = fallback_value
 
     # Round the interpolated values to the specified number of decimal places
-    modified_data = np.around(modified_data, decimals=decimals)
-    return modified_data
+    return np.around(modified_data, decimals=decimals)
