7aeec96b81f1c77ac717920421e98202f368e7f7
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-05-07 22:15:05 +0330

    Improved variable names in mode function

--- proadv/statistics/descriptive.py
+++ proadv/statistics/descriptive.py
@@ -287,4 +287,6 @@ def mode(data):
     """
     values, counts = np.unique(data, return_counts=True)  # Unique values ​​and replicate counts are calculated
     max_count = np.argmax(counts)  # Calculate the indices of the maximum values ​​in the count array
-    return values[max_count], counts[max_count]
+    val_mc = values[max_count]  # values mc => max count
+    counts_mc = counts[max_count]  # counts mc => max count
+    return val_mc, counts_mc
