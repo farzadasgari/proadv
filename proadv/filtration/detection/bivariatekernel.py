ff37ec024a964e919c2cac57b53e33db118c858a
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-05-09 12:36:37 +0330

    Added bivariate kernel cutoff docstrings

--- proadv/filtration/detection/bivariatekernel.py
+++ proadv/filtration/detection/bivariatekernel.py
@@ -1,6 +1,31 @@
 import numpy as np
 
 def _cutoff(density_profile, velocity_profile, c1_threshold, c2_threshold, force_profile, peak_index, grid):
+    grid):
+    """
+    Find the lower and upper cutoff velocities based on specified criteria.
+
+    Parameters
+    ------
+        density_profile (array_like): Density profile of the system.
+        velocity_profile (array_like): Velocity profile of the system.
+        c1_threshold (float): Threshold ratio for force compared to peak force.
+        c2_threshold (float): Threshold for absolute change in force.
+        force_profile (array_like): Force profile of the system.
+        peak_index (int): Index of the peak force in the force profile.
+        grid (int): Grid spacing or resolution.
+
+    Returns
+    ------
+        lower_cutoff_velocity, upper_cutoff_velocity: A tuple containing the lower and upper cutoff velocities.
+
+    Note
+    ------
+        This function assumes that the input arrays (density_profile, velocity_profile, and force_profile) 
+            are of the same length.
+
+    """
+    
     profile_length = force_profile.size
     delta_force = np.append([0], np.diff(force_profile)) * grid / density_profile
     
