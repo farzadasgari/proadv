499fb9f732f154ae13aa9e699dc1b41cef66c6f9
Farzad Asgari <farzad.developer13@gmail.com>
2024-05-12 02:17:22 +0330

    Added trivariatekernel cutoff comments

--- proadv/filtration/detection/trivariatekernel.py
+++ proadv/filtration/detection/trivariatekernel.py
@@ -435,6 +435,7 @@ def _cutoff(density_profile, velocity_profile, c1_threshold, c2_threshold, force
     profile_length = force_profile.size
     delta_force = np.append([0], np.diff(force_profile)) * grid / density_profile
 
+    # Find lower cutoff index
     for i in range(peak_index - 1, 0, -1):
         if force_profile[i] / force_profile[peak_index] <= c1_threshold and abs(delta_force[i]) <= c2_threshold:
             lower_cutoff_index = i
@@ -442,6 +443,7 @@ def _cutoff(density_profile, velocity_profile, c1_threshold, c2_threshold, force
     else:
         lower_cutoff_index = 1
 
+    # Find upper cutoff index
     for i in range(peak_index + 1, profile_length - 1):
         if force_profile[i] / force_profile[peak_index] <= c1_threshold and abs(delta_force[i]) <= c2_threshold:
             upper_cutoff_index = i
