308f9a5d9c1d73a9b9e979034e8451d0ef054645
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-05-11 11:34:16 +0330

    Added spectrum docstrings

--- proadv/statistics/signal/spectrum.py
+++ proadv/statistics/signal/spectrum.py
@@ -3,6 +3,22 @@ from scipy.signal.windows import hann
 
 
 def power_spectra(data, sampling_frequency, overlap=50, block=2048, onesided=True):
+    """
+    Compute the power spectra of the input data.
+
+    Parameters
+    ------
+    data (array_like): Input data array.
+    sampling_frequency (float): Sampling frequency of the input data.
+    overlap (int, optional): Percentage overlap between blocks. Default is 50.
+    block (int, optional): Length of each block for computing the spectra. Default is 2048.
+    onesided (bool, optional): If True, return one-sided spectrum. Default is True.
+
+    Returns
+    ------
+    fxu (array_like): Array of frequencies.
+    fyu (array_like): Power spectral density of the input data.
+    """
     win=hann(block, True)
     fxu, fyu = welch(data, sampling_frequency, window=win, noverlap=overlap, nfft=block, return_onesided=onesided)
     return fxu, fyu
