908cdd3e53a47cdc5ce6886a966751960ed0a3e8
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-05-11 14:56:50 +0330

    Added kinetic comments

--- proadv/statistics/signal/fluid.py
+++ proadv/statistics/signal/fluid.py
@@ -14,14 +14,18 @@ def kinetic_turbulent_energy(u, v, w):
     ------
     kinetic (float): Kinetic turbulent energy.
     """
+
+    # Compute fluctuations
     up = u - mean(u)
     vp = v - mean(v)
     wp = w - mean(w)
-    
+
+    # Calculate the mean squared velocities in each direction
     mean_ui2 = mean(up ** 2)
     mean_vi2 = mean(vp ** 2)
     mean_wi2 = mean(wp ** 2)
 
+    # Compute the total kinetic turbulent energy
     kinetic = 0.5 * (mean_ui2 + mean_vi2 + mean_wi2)
 
     return kinetic
