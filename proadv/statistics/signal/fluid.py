da4c2ab752e2866f76f53e1f183ac4a45ff3c630
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-05-11 14:55:22 +0330

    Added kinetic docstrings

--- proadv/statistics/signal/fluid.py
+++ proadv/statistics/signal/fluid.py
@@ -1,7 +1,19 @@
 from proadv.statistics.spread import mean
 
 def kinetic_turbulent_energy(u, v, w):
+    """
+    Compute the kinetic turbulent energy based on velocity components.
 
+    Parameters
+    ------
+    u (array_like): Array containing longitudinal velocity component. 
+    v (array_like): Array containing transverse velocity component. 
+    w (array_like): Array containing vertical velocity component. 
+
+    Returns
+    ------
+    kinetic (float): Kinetic turbulent energy.
+    """
     up = u - mean(u)
     vp = v - mean(v)
     wp = w - mean(w)
@@ -10,6 +22,6 @@ def kinetic_turbulent_energy(u, v, w):
     mean_vi2 = mean(vp ** 2)
     mean_wi2 = mean(wp ** 2)
 
-    k = 0.5 * (mean_ui2 + mean_vi2 + mean_wi2)
+    kinetic = 0.5 * (mean_ui2 + mean_vi2 + mean_wi2)
 
-    return k
+    return kinetic
