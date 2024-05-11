87d75100a5c810bedcf5a42b5a2419e56a6d1660
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-05-11 11:09:52 +0330

    Added pollution rate

---
+++ proadv/filtration/detection/pollution.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def pollution_rate(x, xfil, k):
+    pos = np.nonzero(x > xfil + k * std(x))
+    neg = np.nonzero(x < -xfil - k * std(x))
+    noises = np.concatenate((pos, neg), axis=None)
+    pollution = len(noises) / len(x) * 100
+    return pollution
