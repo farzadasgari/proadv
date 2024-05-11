f220de2f623e57c6016e4e1103cc056991dea745
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-05-11 11:16:44 +0330

    Added pollution rate docstrings

--- proadv/filtration/detection/pollution.py
+++ proadv/filtration/detection/pollution.py
@@ -3,6 +3,19 @@ from proadv.statistics.spread import std
 
 
 def pollution_rate(x, xfil, k):
+    """
+    Compute the pollution rate based on the given data and threshold.
+
+    Parameters
+    ------
+    x (array_like): Input data array.
+    xfil (array_like): SSA analyzed data of x.
+    k (float): Threshold factor.
+
+    Returns
+    ------
+    pollution (float): Pollution rate as a percentage.
+    """
     pos = np.nonzero(x > xfil + k * std(x))
     neg = np.nonzero(x < -xfil - k * std(x))
     noises = np.concatenate((pos, neg), axis=None)
