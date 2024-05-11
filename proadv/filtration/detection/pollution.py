be4cb4f9d5316360a7d9a14d5bc721bb8fbdc41e
Farzad Asgari <95700238+farzadasgari@users.noreply.github.com>
2024-05-11 11:19:33 +0330

    Added pollution rate comments

--- proadv/filtration/detection/pollution.py
+++ proadv/filtration/detection/pollution.py
@@ -16,8 +16,17 @@ def pollution_rate(x, xfil, k):
     ------
     pollution (float): Pollution rate as a percentage.
     """
+    
+    # Find positions where x exceeds the threshold
     pos = np.nonzero(x > xfil + k * std(x))
+    
+    # Find positions where x is below the negative threshold
     neg = np.nonzero(x < -xfil - k * std(x))
+    
+    # Concatenate positive and negative noises
     noises = np.concatenate((pos, neg), axis=None)
+    
+    # Compute pollution rate
     pollution = len(noises) / len(x) * 100
+    
     return pollution
