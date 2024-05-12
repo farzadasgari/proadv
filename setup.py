2102faf98b541b03ccda1e794845f23b3b6273b1
Farzad Asgari <farzad.developer13@gmail.com>
2024-05-12 18:23:42 +0330

    Update setup v2.0.2

--- setup.py
+++ setup.py
@@ -42,8 +42,6 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.8",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Signal Processing",
     ],
     project_urls={
         "Bug Tracker": "https://github.com/farzadasgari/proadv/issues",
