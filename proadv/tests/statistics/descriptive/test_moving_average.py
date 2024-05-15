19e68492ea93a5f5830667b590d08bbe05f2ba1c
Hivayrn <hivahooshyrn792@gmail.com>
2024-05-15 21:25:11 +0330

    test_moving_average_window_size_greater_than_data_size

--- proadv/tests/statistics/descriptive/test_moving_average.py
+++ proadv/tests/statistics/descriptive/test_moving_average.py
@@ -6,3 +6,7 @@ from proadv.statistics.series import moving_average
 def test_moving_average_with_single_value():
     with pytest.raises(ValueError):
         moving_average(np.array([5]))
+
+def test_moving_average_window_size_greater_than_data_size():
+    with pytest.raises(ValueError):
+        moving_average(np.array([1, 2, 3, 4, 5]))
