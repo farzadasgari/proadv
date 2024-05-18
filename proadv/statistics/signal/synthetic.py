540205782d4a4f10bebe49776108fb233701b978
Farzad Asgari <farzad.developer13@gmail.com>
2024-05-18 23:25:33 +0330

    Added synthetic noise generator

--- proadv/statistics/signal/synthetic.py
+++ proadv/statistics/signal/synthetic.py
@@ -12,3 +12,20 @@ def _random_index(ndata, percent):
         else:
             r.append(rn)
     return np.array(sorted(r))  # Random Indexes
+
+
+def synthetic_noise(data, percent):
+    synthetic_data = _random_index(data.size, percent)
+
+    r = np.random.normal(0, 0.05, data.size)  # Nosie Vector
+
+    s = np.zeros(data.size)  # Artificial Spike
+
+    for k in range(data.size):
+        if k % 2 == 0:
+            s[synthetic_data[k]] = (abs(r[k]) + 0.8) * mean(data)
+        else:
+            s[synthetic_data[k]] = (-abs(r[k]) - 0.8) * mean(data)
+
+    synthetic_polluted_data = data + synthetic_data
+    return synthetic_polluted_data
