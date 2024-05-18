657137f2e5a39bb19e363ec747781326a180b7b1
Farzad Asgari <farzad.developer13@gmail.com>
2024-05-18 23:48:14 +0330

    Added artificial pollution example comments

--- examples/signal/synthetic.py
+++ examples/signal/synthetic.py
@@ -4,10 +4,16 @@ import matplotlib.pyplot as plt
 
 
 def main():
+    """Generate and plot synthetic polluted data."""
     np.random.seed(0)
+
+    # Generate random data
     data = np.random.randint(0, 50, 500)
+
+    # Generate synthetic polluted data
     artificial_pollution = adv.statistics.signal.synthetic.synthetic_noise(data, percent=10)
 
+    # Plot the synthetic polluted data and the original data
     plt.plot(artificial_pollution, color='crimson', label='Synthetic Pollution')
     plt.plot(data, label="Main Data")
     plt.legend(loc='upper right')
