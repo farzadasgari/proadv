312242492341eaf68171566e955af8d9f3f542b9
Mojtaba Karimi <93880997+mojikarimi@users.noreply.github.com>
2024-05-19 03:00:20 +0330

    Construction of function low_to_band

--- proadv/statistics/signal/butter.py
+++ proadv/statistics/signal/butter.py
@@ -71,3 +71,60 @@ def low_to_stop(zero, poles, system, pc=1.0, pw=1.0):
     systemb = system * real(prod(-zero) / prod(-poles))
 
     return zerob, polesb, systemb
+
+def low_to_band(zero, poles, system, pc=1.0, pw=1.0):
+    """
+    This function converts a LP(low-pass) filter prototype to a BP(band-pass) filter.
+
+    Parameters
+    ------
+    zero (array_like) : Zeros of the analog filter transfer function.
+    poles (array_like) : Poles of the analog filter transfer function.
+    system (float) : System gain of the analog filter transfer function.
+    pc (float) : Desired passband center, as angular frequency.
+    pw (float) : Desired passband width, as angular frequency.
+
+    Returns
+
+    zerob (array_like): Zeros of the transformed BP(band-pass) filter transfer function.
+    polesb (array_like): Poles of the transformed BP(band-pass) filter transfer function.
+    systemb (float) : System gain of the transformed BP(band-pass) filter.
+    ------
+
+    Examples
+    ------
+    >>> zero = [6 + 3j, 6 - 3j]
+    >>> poles = [8, -20]
+    >>> system = 0.5
+    >>> pc = 0.53
+    >>> pw = 14
+    >>> low_to_band(zero, poles, system, pc, pw)
+    (array([8.39973247e+01+4.20013377e+01j, 8.39973247e+01-4.20013377e+01j,
+           2.67525513e-03-1.33771277e-03j, 2.67525513e-03+1.33771277e-03j]), array([ 1.11997492e+02+0.j, -1.00321788e-03+0.j,  2.50809188e-03+0.j,
+           -2.79998997e+02+0.j]), 0.5)
+    """
+    zero = atleast_1d(zero)
+    poles = atleast_1d(poles)
+    fpc = float(pc)
+    fpw = float(pw)
+
+    scale = _relative_scale(zero, poles)
+    # Scale poles and zeros to desired bandwidth
+    lf = lambda a, b: a * b / 2
+    zerol = lf(zero, fpw)
+    polesl = lf(poles, fpw)
+
+    # Square root needs to produce complex result, not NaN
+    zerol = zerol.astype(complex)
+    polesl = polesl.astype(complex)
+
+    # Duplicate poles and zeros and shift from baseband to +pc and -pc
+    zerob = _catenate(zerol, fpc)
+    polesb = _catenate(polesl, fpc)
+
+    zerob = append(zerob, zeros(scale))
+
+    # Cancel out gain change from frequency scaling
+    systemb = system * fpw ** scale
+
+    return zerob, polesb, systemb
